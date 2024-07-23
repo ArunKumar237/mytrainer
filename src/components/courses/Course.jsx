a5f74a2af0974625fd1b0ea4794fba37ceb4b7c6
ArunKumar237 <arunchary237@gmail.com>
2024-07-24 00:34:22 +0530

    Courses page done including mobile responsive

--- src/components/courses/Course.jsx
+++ src/components/courses/Course.jsx
@@ -1,72 +1,86 @@
 import React from 'react'
 import Stars from "../home/coursesoffering/Stars";
+import "./Course.css";
 
 const Course = (props) => {
   return (
-    <div className={`${props.classname} container-md my-4`}>
-      <div className="row">
-        <div className="col-3 d-flex justify-content-center">
-          <img className="rounded-3" style={{ width: "251px", height: "350px" }} src={props.posterImgSrc} alt="" />
-        </div>
-        <div className="col-9 rounded-3 border border-1 bg-white">
-          <div style={{ height: "3rem", background: "linear-gradient(90deg, rgba(0,99,255,1) 0%, rgba(35,171,80,1) 100%)" }} className="row mb-4 rounded-top title-bg d-flex align-items-center" data-before={"Next batch - 23rd May 2024, Batch-65"}>
-            <h5 className="ps-5 text-white">{props.courseTitle}</h5>
+    <div style={{ height: '100vh' }} className={`${props.classname} my-4`}>
+      <div className="container h-md-50 h-lg-75">
+        <div className="row h-100">
+          <div className="col-md-3 col-lg-2 d-flex justify-content-center h-100">
+            <img
+              className="rounded-3"
+              style={{ height: '75%' }}
+              src={props.posterImgSrc}
+              alt=""
+            />
           </div>
-          <div className="row mb-4">
-            <div className="col-9">
-              {props.courseDesc}
+          <div className="col-md-9 col-lg-10 d-flex flex-column gap-1 bg-white">
+            <div style={{ height: "3rem", background: "linear-gradient(90deg, rgba(0,99,255,1) 0%, rgba(35,171,80,1) 100%)" }} className="row rounded-top title-bg d-flex align-items-center" data-before={"Next batch - 23rd May 2024, Batch-65"}>
+              <h5 className="ps-5 text-white">{props.courseTitle}</h5>
             </div>
-            <div className="col-3 text-center">
-              <h1 style={{ color: "#23ab50" }} className="fs-1 fw-bold">{`₹${props.coursePrice}/-`}</h1>
-            </div>
-          </div>
-          <div className="row my-4">
-            <div className="col-11 mx-auto">
-              <div style={{ backgroundColor: "#e5e5e5" }} className="row py-2 px-4">
-                <div className="col-7 py-4">
-                  <div className="row">
-                    <div className="col d-flex gap-3">
-                      <p className="fw-semibold">Trainer:</p>
-                      <div className="d-flex gap-2">
-                        <img style={{ height: "30px", width: "30px" }} className="rounded-circle" src={props.trainerPic} alt="" />
-                        <div className="col d-flex flex-column gap-1 mt-1 p-0">
-                          <p style={{ fontSize: "14px" }} className="m-0 text-decoration-underline">{props.trainerName}</p>
-                          <p style={{ fontSize: "14px" }} className="m-0 fw-bold">{props.trainerQuali}</p>
-                          <p style={{ fontSize: "14px" }} className="m-0">{props.trainerRole}</p>
-                        </div>
-                      </div>
-                    </div>
+            <div className="row d-flex mx-auto justify-content-center">
+              <div className="col-11 p-0">
+                <div className="row gap-md-2 justify-content-lg-between">
+                  <div style={{ fontSize: ".9rem" }} className="col-8 p-0">
+                    {props.courseDesc}
                   </div>
-                  <div className="row d-flex gap-1 flex-column py-4">
-                    <Stars />
-                    <p className="fw-bold">Schedule: {props.courseSchedule}</p>
-                  </div>
-                </div>
-                <div className="col-5 text-center py-4">
-                  <p style={{ fontSize: "14px" }} className="py-2 px-3 bg-primary rounded-3 text-white">
-                    Duration: {props.courseDuration} months
-                  </p>
-                  <p style={{ fontSize: "14px" }} className="py-2 px-3 bg-primary rounded-3 text-white">
-                    Class Schedule: {props.classSchedule}
-                  </p>
-                  <p style={{ fontSize: "14px" }} className="py-2 px-3 bg-primary rounded-3 text-white">
-                    Class Time: {props.classTime} IST
-                  </p>
-                  <div className="d-flex align-items-center gap-2">
-                    <p style={{ fontSize: "14px" }} className="m-0 fw-semibold">Course Sample:</p>
-                    <img
-                      style={{ width: "40px", height: "40px" }}
-                      src={props.courseSampleLink}
-                      alt=""
-                    />
+                  <div className="col-2 text-center p-0 me-lg-3">
+                    <p style={{ color: "#23ab50", fontSize: "2rem" }} className="fw-bold">{`₹${props.coursePrice}/-`}</p>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-11 mx-auto">
-              <p style={{ backgroundColor: "#23ab50" }} className="col-4 py-2 rounded-3 text-white text-center ms-auto">Register</p>
+
+
+            <div className="row">
+              <div className="col-11 mx-auto">
+                <div style={{ backgroundColor: "#e5e5e5" }} className="row rounded p-md-2 p-lg-4 ">
+                  <div className="col-lg-6 col-md-5 text-md-center text-lg-start">
+                    <div className="row">
+                      <div className="col d-flex flex-md-column flex-lg-row gap-2 gap-md-0  align-items-md-center align-items-lg-start">
+                        <p className="fw-semibold mb-md-0 me-lg-2">Trainer:</p>
+                        <div className="d-flex gap-lg-1 gap-md-3 align-items-lg-start align-items-md-center">
+                          <img style={{ height: "1.875rem", width: "1.875rem" }} className="rounded-circle" src={props.trainerPic} alt="" />
+                          <div className="col d-flex flex-column gap-lg-1 gap-md-0 mt-1 p-0">
+                            <p style={{ fontSize: "0.875rem" }} className="m-0 text-decoration-underline">{props.trainerName}</p>
+                            <p style={{ fontSize: "0.8rem" }} className="m-0 fw-bold">{props.trainerQuali}</p>
+                            <p style={{ fontSize: "0.8rem" }} className="m-0">{props.trainerRole}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row d-flex gap-1 flex-column">
+                      <Stars />
+                      <p style={{ fontSize: "0.875rem" }} className="fw-bold m-0">Schedule: {props.courseSchedule}</p>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-7 text-center d-flex flex-column gap-2">
+                    <p style={{ fontSize: "0.875rem" }} className="m-0 p-md-1 p-lg-2 bg-primary rounded text-white">
+                      Duration: {props.courseDuration} months
+                    </p>
+                    <p style={{ fontSize: "0.875rem" }} className="m-0 p-md-1 p-lg-2 bg-primary rounded text-white">
+                      Class Schedule: {props.classSchedule}
+                    </p>
+                    <p style={{ fontSize: "0.875rem" }} className="m-0 p-md-1 p-lg-2 bg-primary rounded text-white">
+                      Class Time: {props.classTime} IST
+                    </p>
+                    <div className="d-flex gap-2">
+                      <p style={{ fontSize: "0.875rem" }} className="m-0 fw-semibold">Course Sample:</p>
+                      <img
+                        style={{ width: "1.5rem", height: "1.5rem" }}
+                        src={props.courseSampleLink}
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-11 mx-auto">
+                <div className="row">
+                  <div className="col-4 ms-auto my-2 btn btn-success btn-sm">Register</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import "./Courses.css";
 import Course from "./Course";
 import Sectionbreak from "../home/Sectionbreak"
 import Coursesmob from "./Coursesmob";
@@ -10,7 +9,7 @@ const data = [
     classname: "d-none d-md-block",
     posterImgSrc: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
     courseTitle: "Fullstack Python",
-    courseDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda.",
+    courseDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda. ",
     coursePrice: "18000",
     trainerPic: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
     trainerName: "D. Chaitanya Reddy",
@@ -22,40 +21,40 @@ const data = [
     classTime: "7:30 am - 9:00 am",
     courseSampleLink: "https://img.icons8.com/color/96/youtube-play.png"
   },
-  {
-    id:2,
-    classname: "d-none d-md-block",
-    posterImgSrc: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
-    courseTitle: "Fullstack Python",
-    courseDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda.",
-    coursePrice: "18000",
-    trainerPic: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
-    trainerName: "D. Chaitanya Reddy",
-    trainerQuali: "IIT Roorkee Alumni",
-    trainerRole: "Senior Data Scientist",
-    courseSchedule: "Coming Soon",
-    courseDuration: "1.5",
-    classSchedule: "Monday - Saturday",
-    classTime: "7:30 am - 9:00 am",
-    courseSampleLink: "https://img.icons8.com/color/96/youtube-play.png"
-  },
-  {
-    id:3,
-    classname: "d-none d-md-block",
-    posterImgSrc: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
-    courseTitle: "Fullstack Python",
-    courseDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda.",
-    coursePrice: "18000",
-    trainerPic: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
-    trainerName: "D. Chaitanya Reddy",
-    trainerQuali: "IIT Roorkee Alumni",
-    trainerRole: "Senior Data Scientist",
-    courseSchedule: "Coming Soon",
-    courseDuration: "1.5",
-    classSchedule: "Monday - Saturday",
-    classTime: "7:30 am - 9:00 am",
-    courseSampleLink: "https://img.icons8.com/color/96/youtube-play.png"
-  },
+  // {
+  //   id:2,
+  //   classname: "d-none d-md-block",
+  //   posterImgSrc: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
+  //   courseTitle: "Fullstack Python",
+  //   courseDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda.",
+  //   coursePrice: "18000",
+  //   trainerPic: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
+  //   trainerName: "D. Chaitanya Reddy",
+  //   trainerQuali: "IIT Roorkee Alumni",
+  //   trainerRole: "Senior Data Scientist",
+  //   courseSchedule: "Coming Soon",
+  //   courseDuration: "1.5",
+  //   classSchedule: "Monday - Saturday",
+  //   classTime: "7:30 am - 9:00 am",
+  //   courseSampleLink: "https://img.icons8.com/color/96/youtube-play.png"
+  // },
+  // {
+  //   id:3,
+  //   classname: "d-none d-md-block",
+  //   posterImgSrc: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
+  //   courseTitle: "Fullstack Python",
+  //   courseDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae sed ducimus quas alias facere distinctio fuga! Laudantium autem assumenda.",
+  //   coursePrice: "18000",
+  //   trainerPic: "https://img.freepik.com/free-photo/lions-digital-art-style_23-2150977775.jpg?t=st=1721479693~exp=1721483293~hmac=8dd1f8a39a6591fcf753d0619c87b7aa1c7657d70a940bb5673192ba46ade4dd&w=360",
+  //   trainerName: "D. Chaitanya Reddy",
+  //   trainerQuali: "IIT Roorkee Alumni",
+  //   trainerRole: "Senior Data Scientist",
+  //   courseSchedule: "Coming Soon",
+  //   courseDuration: "1.5",
+  //   classSchedule: "Monday - Saturday",
+  //   classTime: "7:30 am - 9:00 am",
+  //   courseSampleLink: "https://img.icons8.com/color/96/youtube-play.png"
+  // },
   {
     id:4,
     classname: "d-md-none",
